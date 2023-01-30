d2b4df5f67b86a050a162d66ef9e8aaffda94761
vikaskhare0905 <vikaskharevk1994@gmail.com>
2023-01-30 19:50:12 +0530

    changes by Vikas

---
+++ src/main/java/com/BikkadIT/DI/GitUse4/GitUse4Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.DI.GitUse4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUse4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUse4Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/BikkadIT/DI/GitUse4/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.DI.GitUse4;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GitUse4Application.class);
+	}
+
+}
